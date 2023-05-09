965cd673d46fe1b0466bf38f8ea139a4fae3df03
TaherJalal <taherjalaljuma@gmail.com>
2023-05-09 10:48:13 +0300

    signup page done ??

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -11,75 +11,78 @@ export default async function signup(
     res.status(405).send("Method Not Allowed, Not A POST Request");
   }
 
-  const { firstName, lastName, email, password } = req.body
-  const salt = process.env.SALT
-  const secret: string= process.env.SECRET as string
+  const { firstName, lastName, email, password } = req.body;
+  const salt = process.env.SALT;
+  const secret: string = process.env.SECRET as string;
 
   const checkEmail = await prisma.user.findUnique({
-    where:{
-        email
-    }
-  })
+    where: {
+      email,
+    },
+  });
 
-  if(checkEmail){
-    res.status(400).send("Email ALready Used")
+  if (checkEmail) {
+    res.status(400).send("Email ALready Used");
   }
 
-  const errors = []
+  const errors = [];
 
-  if(!firstName){
-    errors.push("First Name Is required")
+  if (!firstName) {
+    errors.push("First Name Is required");
   }
 
-  if(firstName.length >  20 && firstName.length < 3){
-    errors.push("First Name Characters Should Be Graeter Than 3 And Less Than 20")
+  if (firstName.length > 20 && firstName.length < 3) {
+    errors.push(
+      "First Name Characters Should Be Graeter Than 3 And Less Than 20"
+    );
   }
 
-  if(!lastName){
-    errors.push("Last Name Is Required")
+  if (!lastName) {
+    errors.push("Last Name Is Required");
   }
 
-  if(lastName.length >  20 && lastName.length < 3){
-    errors.push("First Name Characters Should Be Graeter Than 3 And Less Than 20")
+  if (lastName.length > 20 && lastName.length < 3) {
+    errors.push(
+      "First Name Characters Should Be Graeter Than 3 And Less Than 20"
+    );
   }
 
-  if(!email){
-    errors.push("Email Is Required")
+  if (!email) {
+    errors.push("Email Is Required");
   }
 
-  if(!email.includes("@") || !email.includes(".com")){
-    errors.push("Not A Valid Email Address")
+  if (!email.includes("@") || !email.includes(".com")) {
+    errors.push("Not A Valid Email Address");
   }
 
-  if(!password){
-    errors.push("Password Is Required")
+  if (!password) {
+    errors.push("Password Is Required");
   }
 
-  if(password.length < 8 || password.length > 22){
-    errors.push("Password Must Be Between 8 To 22 Characters Long")
+  if (password.length < 8 || password.length > 22) {
+    errors.push("Password Must Be Between 8 To 22 Characters Long");
   }
 
-//   if(!password.includes("@") || !password.includes(".") || !password.includes("!")){
-//     errors.push("Password Must Contain Special Characters")
-//   }
+  //   if(!password.includes("@") || !password.includes(".") || !password.includes("!")){
+  //     errors.push("Password Must Contain Special Characters")
+  //   }
 
-  if(errors.length > 0){
-    res.status(400).send(errors)
+  if (errors.length > 0) {
+    res.status(400).send(errors);
   }
 
-  const encPassword = bcrypt.hashSync(password , 10)
+  const encPassword = bcrypt.hashSync(password, 10);
 
   const user = await prisma.user.create({
-    data :{
-        firstName,
-        lastName,
-        email,
-        password: encPassword
-    }
-  })
+    data: {
+      firstName,
+      lastName,
+      email,
+      password: encPassword,
+    },
+  });
 
-  const token = jwt.sign(user.id , secret)
+  const token = jwt.sign(user.id, secret);
 
-  res.status(200).json(token)
-
+  res.status(200).json({ token: token });
 }
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import React, { useState , useEffect } from 'react'
 
 function signup() {
+
+  const [firstName , setFirstName] = useState<string>()
+  const [lastName , setLastName] = useState<string>()
+  const [email , setEmail] = useState<string>()
+  const [password , setPassword] = useState<string>()
+  const [token , setToken] = useState<string>()
+
+  useEffect(() => {
+    console.log(token)
+  }, [token])
+  
+
+const register = async (e:any) => {
+  e.oreventDefault()
+  const { data } = await axios.post("http://localhost:3000/api/auth/signup" , {
+  firstName,
+  lastName,
+  email,
+  password
+  })
+
+ console.log(data)
+
+}
+
   return (
-    <div>signup</div>
+    <div className='dark:bg-neutral-950 dark:text-white h-screen w-screen font-montserrat flex justify-center items-center'>
+
+<form className='w-2/4 flex flex-col gap-4' onSubmit={register}>
+        <h2 className='text-3xl'>Sign Up</h2>
+
+          <div className='flex flex-col'>
+          <h3 className='text-xl'>First Name</h3>
+          <input type="text" className='text-black' onChange={(e) => setFirstName(e.target.value)}/>
+          </div>
+
+          <div className='flex flex-col'>
+          <h3 className='text-xl'>Last Name</h3>
+          <input type="text" className='text-black' onChange={(e) => setLastName(e.target.value)}/>
+          </div>
+
+          <div className='flex flex-col'>
+          <h3 className='text-xl'>Email Address</h3>
+          <input type="email" className='text-black' onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+
+          <div className='flex flex-col'>
+          <h3 className='text-xl'>Password</h3>
+          <input type="password" className='text-black' onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+
+        <div className='flex gap-2'>
+          <p className='capitalize text-sm'>have an account?</p><Link href="/auth/signin">Login Here</Link>
+        </div>
+          <button type='submit' className='text-xl'>Register</button>
+      </form>
+
+    </div>
   )
 }
 
