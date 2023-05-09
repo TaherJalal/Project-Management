a275aad5c97ae10f0377114be10384226a5145d0
TaherJalal <taherjalaljuma@gmail.com>
2023-05-09 15:50:12 +0300

    added sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Link from 'next/link'
-import {AiOutlineHome, AiOutlineArrowDown , AiOutlineArrowUp , AiOutlinePlus} from "react-icons/ai"
-
+import {RxHamburgerMenu} from 'react-icons/rx'
+import {AiOutlineHome, AiOutlineArrowDown , AiOutlineArrowUp , AiOutlineArrowLeft , AiOutlinePlus} from "react-icons/ai"
+import {HiOutlineArrowLeft} from 'react-icons/hi'
 function index() {
 
   const [token ,setToken] = useState<string>("")
   const [state , setState] = useState<boolean>(false)
+  const [showSideBar , setShowSideBar] = useState<boolean>(false)
   const [spaces ,setSpaces] = useState([])
   const [newSpaceState , setNewSpaceState] = useState(false)
 
@@ -25,9 +27,24 @@ function index() {
    token ? 
    (
     <div>
-        <div className='h-screen w-72 bg-rose-300 flex flex-col font-montserrat'>
+      {
+        !showSideBar ? 
+        (
+          <div className='w-20 h-screen flex justify-center py-3 bg-rose-300' onClick={() => setShowSideBar(true)}>
+            <div className='flex justify-end'>
+            <RxHamburgerMenu size={40}/>
+            </div>
+          </div>
+        )
+        :
+        (
+          <div className='h-screen w-72 bg-rose-300 flex flex-col font-montserrat'>
 
           <div className='p-3 pl-6 flex flex-col gap-4 text-2xl font-semibold'>
+<div className='flex justify-end'>
+
+            <HiOutlineArrowLeft size={40} onClick={() => setShowSideBar(false)}/>
+</div>
 
             <div className='flex gap-2 justify-center items-center'>
           <Link href="/" >Home</Link>
@@ -73,16 +90,15 @@ function index() {
             }
             
             </div>
-          </div>
-
-
-          <div> 
 
           </div>
         
           </div>
 
         </div>
+        )
+      }
+
     </div>
    )
    
