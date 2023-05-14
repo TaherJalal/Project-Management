ff650963bad4f931646545cc62831009bfd6f722
TaherJalal <taherjalaljuma@gmail.com>
2023-05-14 21:10:51 +0300

    change color navbar

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,23 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
-    <div className='font-montserrat font-medium text-lg flex justify-between px-5 py-1 dark:bg-neutral-950 dark:text-white text-black bg-emerald-200'>
+    <div className="font-montserrat font-medium text-lg flex justify-between px-5 py-1 dark:bg-zinc-900 dark:text-white text-black bg-emerald-200">
+      <div className="flex gap-5 font-montserrat">
+        <Link href="/">Home</Link>
+        <Link href="/space">Spaces</Link>
+        <Link href="/">Blah</Link>
+      </div>
 
-    <div className='flex gap-5 font-montserrat'>
-  <Link href="/">Home</Link>
-  <Link href="/space">Spaces</Link>
-  <Link href="/">Blah</Link>
+      <div className="flex gap-5 font-montserrat">
+        <Link href="/about">About Us</Link>
+        <Link href="auth/signin">Register/Login</Link>
+      </div>
     </div>
-
-  <div className='flex gap-5 font-montserrat'>
-  <Link href="/about">About Us</Link>
-  <Link href="auth/signin">Register/Login</Link>
-  </div>
-
-  </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
