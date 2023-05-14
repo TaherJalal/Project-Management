04753cc90d599269465828f57e5b63231c6c6208
TaherJalal <taherjalaljuma@gmail.com>
2023-05-14 20:39:26 +0300

    add diffrent pages for auth and not auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,9 +26,13 @@ export default function index() {
   const [spaces, setSpaces] = useState([]);
   const [invites, setInvites] = useState([]);
   const [spaceName, setSpaceName] = useState<string>("");
-  const [acceptInvite, setAcceptInvite] = useState<boolean>(false);
-  const [inviteId, setInviteId] = useState<string>("");
-  const [spaceId, setSpaceId] = useState<String>("");
+  const [token, setToken] = useState<string>("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setToken(localStorage.getItem("token")!);
+    }
+  }, []);
 
   const { isLoading, error } = useQuery({
     queryKey: ["landingPageData"],
@@ -43,6 +47,15 @@ export default function index() {
         setInvites(data.data.invites);
     },
   });
+
+  if (!token) {
+    return (
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+    );
+  }
 
   const newSpace = () => {
     axios.post(
@@ -73,22 +86,19 @@ export default function index() {
   };
 
   const RejectInvite = (inviteId: string, spaceId: string) => {
-    console.log(acceptInvite),
-      axios.post(
-        "http://localhost:3000/api/user/invites",
-        {
-          acceptInvite,
-          inviteId,
-          spaceId,
-        },
-        {
-          headers: { Authorization: localStorage.getItem("token") },
-        }
-      );
+    axios.post(
+      "http://localhost:3000/api/user/invites",
+      {
+        acceptInvite: false,
+        inviteId,
+        spaceId,
+      },
+      {
+        headers: { Authorization: localStorage.getItem("token") },
+      }
+    );
     window.location.reload();
   };
-
-  console.log(data);
 
   if (isLoading)
     return (
@@ -106,149 +116,138 @@ export default function index() {
 
   return (
     <>
-      {localStorage.getItem("token") ? (
-        <div className="bg-indigo-200 flex overflow-hidden">
-          {!showSideBar ? (
-            <div
-              className="w-10 h-screen flex justify-center py-3 bg-rose-300"
-              onClick={() => setShowSideBar(true)}
-            >
-              <div className="flex justify-end">
-                <RxHamburgerMenu size={20} />
-              </div>
-            </div>
-          ) : (
-            <div className="h-screen w-auto bg-rose-300 flex flex-col font-montserrat">
-              <div className="p-2 flex flex-col gap-4 text-lg font-semibold">
-                <div className="flex justify-end">
-                  <HiOutlineArrowLeft
-                    size={20}
-                    onClick={() => setShowSideBar(false)}
-                  />
-                </div>
-
-                <div className="flex gap-2 justify-center items-center">
-                  <h2 className="">Hi, {name} !</h2>
-                </div>
-
-                <div className="flex gap-2 justify-center items-center">
-                  <Link href="/">Home</Link>
-                  <AiOutlineHome />
-                </div>
-
-                <div className="flex flex-col gap-2 justify-center items-center">
-                  <div
-                    className="flex gap-2 justify-center items-center"
-                    onClick={
-                      state ? () => setState(false) : () => setState(true)
-                    }
-                  >
-                    <p>Spaces</p>
-                    <RxCardStackMinus />
-                    {state ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />}
-                  </div>
-                  {state ? (
-                    <div>
-                      {spaces.map((space) => (
-                        <div className="text-xs flex" key={space.id}>
-                          <p className="hidden">{space.id}</p>
-                          <p>{space.name}</p>
-                        </div>
-                      ))}
-                    </div>
-                  ) : (
-                    <></>
-                  )}
-                  <div
-                    className="flex gap-2 justify-center items-center"
-                    onClick={
-                      newSpaceState
-                        ? () => setNewSpaceState(false)
-                        : () => setNewSpaceState(true)
-                    }
-                  >
-                    <AiOutlinePlus />
-                    {newSpaceState ? (
-                      <form className="text-black" onSubmit={newSpace}>
-                        <input
-                          type="text"
-                          placeholder="Space Name"
-                          className="text-sm rounded-sm text-center"
-                          onChange={(e) => setSpaceName(e.target.value)}
-                        />
-                      </form>
-                    ) : (
-                      <></>
-                    )}
-                  </div>
-                </div>
-
-                <div className="flex flex-col gap-2 justify-center items-center">
-                  <div
-                    className="flex gap-2 justify-center items-center"
-                    onClick={
-                      state2 ? () => setState2(false) : () => setState2(true)
-                    }
-                  >
-                    <p>Invites</p>
-                    <FcInvite />
-                    {state2 ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />}
-                  </div>
-                  {state2 ? (
-                    <div>
-                      {invites.map((invite) => (
-                        <div className="text-xs flex gap-3" key={invite.id}>
-                          <p className="hidden">{invite.id}</p>
-                          <p>{invite.spaceName}</p>
-                          <p>|</p>
-                          <p className="text-semibold">
-                            {invite.createdByUser}
-                          </p>
-                          <div className="flex gap-2">
-                            <BsCheck2
-                              onClick={() =>
-                                AcceptInvite(invite?.id, invite?.spaceId)!
-                              }
-                            />
-                            <BsXLg
-                              onClick={() =>
-                                RejectInvite(invite?.id, invite?.spaceId)!
-                              }
-                            />
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  ) : (
-                    <></>
-                  )}
-                </div>
-              </div>
-            </div>
-          )}
-
-          <div className="flex flex-col gap-4 h-screen p-3">
-            <div className="flex gap-4 justify-first text-2xl relative left-full">
-              <p>search</p>
-              <p>sort</p>
-              <p>filters</p>
-            </div>
-
-            <div className="flex flex-wrap gap-10 pt-3 pl-3 justify-center items-center">
-              <div className="w-60 h-72 bg-purple-400"></div>
-
-              <div className="w-60 h-72 bg-purple-400"></div>
-
-              <AiOutlinePlus size={40} />
+      <div className="bg-indigo-200 flex overflow-hidden">
+        {!showSideBar ? (
+          <div
+            className="w-10 h-screen flex justify-center py-3 bg-rose-300"
+            onClick={() => setShowSideBar(true)}
+          >
+            <div className="flex justify-end">
+              <RxHamburgerMenu size={20} />
             </div>
           </div>
+        ) : (
+          <div className="h-screen w-auto bg-rose-300 flex flex-col font-montserrat">
+            <div className="p-2 flex flex-col gap-4 text-lg font-semibold">
+              <div className="flex justify-end">
+                <HiOutlineArrowLeft
+                  size={20}
+                  onClick={() => setShowSideBar(false)}
+                />
+              </div>
+
+              <div className="flex gap-2 justify-center items-center">
+                <h2 className="">Hi, {name} !</h2>
+              </div>
+
+              <div className="flex gap-2 justify-center items-center">
+                <Link href="/">Home</Link>
+                <AiOutlineHome />
+              </div>
+
+              <div className="flex flex-col gap-2 justify-center items-center">
+                <div
+                  className="flex gap-2 justify-center items-center"
+                  onClick={state ? () => setState(false) : () => setState(true)}
+                >
+                  <p>Spaces</p>
+                  <RxCardStackMinus />
+                  {state ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />}
+                </div>
+                {state ? (
+                  <div>
+                    {spaces.map((space) => (
+                      <div className="text-xs flex" key={space.id}>
+                        <p className="hidden">{space.id}</p>
+                        <p>{space.name}</p>
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <></>
+                )}
+                <div
+                  className="flex gap-2 justify-center items-center"
+                  onClick={
+                    newSpaceState
+                      ? () => setNewSpaceState(false)
+                      : () => setNewSpaceState(true)
+                  }
+                >
+                  <AiOutlinePlus />
+                  {newSpaceState ? (
+                    <form className="text-black" onSubmit={newSpace}>
+                      <input
+                        type="text"
+                        placeholder="Space Name"
+                        className="text-sm rounded-sm text-center"
+                        onChange={(e) => setSpaceName(e.target.value)}
+                      />
+                    </form>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-2 justify-center items-center">
+                <div
+                  className="flex gap-2 justify-center items-center"
+                  onClick={
+                    state2 ? () => setState2(false) : () => setState2(true)
+                  }
+                >
+                  <p>Invites</p>
+                  <FcInvite />
+                  {state2 ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />}
+                </div>
+                {state2 ? (
+                  <div>
+                    {invites.map((invite) => (
+                      <div className="text-xs flex gap-3" key={invite.id}>
+                        <p className="hidden">{invite.id}</p>
+                        <p>{invite.spaceName}</p>
+                        <p>|</p>
+                        <p className="text-semibold">{invite.createdByUser}</p>
+                        <div className="flex gap-2">
+                          <BsCheck2
+                            onClick={() =>
+                              AcceptInvite(invite?.id, invite?.spaceId)!
+                            }
+                          />
+                          <BsXLg
+                            onClick={() =>
+                              RejectInvite(invite?.id, invite?.spaceId)!
+                            }
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+
+        <div className="flex flex-col gap-4 h-screen p-3">
+          <div className="flex gap-4 justify-first text-2xl relative left-full">
+            <p>search</p>
+            <p>sort</p>
+            <p>filters</p>
+          </div>
+
+          <div className="flex flex-wrap gap-10 pt-3 pl-3 justify-center items-center">
+            <div className="w-60 h-72 bg-purple-400"></div>
+
+            <div className="w-60 h-72 bg-purple-400"></div>
+
+            <AiOutlinePlus size={40} />
+          </div>
         </div>
-      ) : (
-        <div>
-          <Navbar />
-          <Hero />
-        </div>
-      )}
+      </div>
     </>
   );
 }
